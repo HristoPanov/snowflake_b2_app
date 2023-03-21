14f5cf98479a8e8c9fa47b6f348e158d797cce08
HristoPanov <hristopanov@gmail.com>
2023-03-21 13:20:16 +0200

    Update streamlit_app.py
    Added some icons and menu content. Added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,9 @@ import streamlit
 
 streamlit.title("Diner 01")
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 something")
-streamlit.text("Smoothie something")
+streamlit.text("🥣 Omega 3 something")
+streamlit.text("🥗 Kale, Spinach and Smoothie something")
+streamlit.text("🐔 Hard Boiled Eggs something")
+streamlit.text("🥑🍞 Avocado and Bread something")
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
