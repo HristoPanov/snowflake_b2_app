930029939218d3681dbe4a23fcd0ac65b641cd1d
HristoPanov <hristopanov@gmail.com>
2023-03-22 12:33:02 +0200

    Update streamlit_app.py
    Separate the fruit value from the URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fuityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json()) # just text display
 #normalize json response
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
