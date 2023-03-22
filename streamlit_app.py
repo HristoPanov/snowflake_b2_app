fc015502622e2ba85f14f262756ef6ca9ee1e583
HristoPanov <hristopanov@gmail.com>
2023-03-22 12:38:23 +0200

    Update streamlit_app.py
    Remove typing errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ import requests
 #display the table
 #streamlit.dataframe(fruityvice_normalized)
 
-fruit_choise = streamlit.text_input('What fruit would you like information aboyt?','Kiwi')
+fruit_choise = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered',fruit_choise)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choise)
 #normalize json response
