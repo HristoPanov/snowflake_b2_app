ecf6f1f1344cd097528d57133e2d797c6b9111f1
HristoPanov <hristopanov@gmail.com>
2023-03-22 16:29:15 +0200

    Update streamlit_app.py
    Reorganized upload list

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,21 @@ except URLError as e:
   streamlit.error()
 streamlit.write('The user entered',fruit_choice)
 
+streamlit.header("The fruit load list contains:")
+#snowflake related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+ 
+#add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows=get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+
+
+
 #don't run anything past here while troubleshouting
 streamlit.stop()
 
