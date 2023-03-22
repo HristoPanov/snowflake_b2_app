07d0c8ef0cf02b333d42dd16f1e6185bc5af3283
HristoPanov <hristopanov@gmail.com>
2023-03-22 15:46:59 +0200

    Update streamlit_app.py
    Added second entry list from text entry input

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,3 +49,8 @@ my_data_rows = my_cur.fetchall()
 #streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# add another entry list
+fruit_choise_2 = streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ',fruit_choise_2)
+
